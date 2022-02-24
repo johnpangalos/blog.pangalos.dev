ba362fdfdc5987877786832301a7ce46df99a71f
John Pangalos <john@pangalos.dev>
2022-02-24 12:18:56 +0100

    Add in question mark

--- src/hooks.ts
+++ src/hooks.ts
@@ -15,7 +15,7 @@ export function getSession(event: RequestEvent): App.Session {
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event, {
     transformPage: ({ html }) => {
-      const isDark = event.request.headers.get("cookie").includes("dark");
+      const isDark = event.request.headers.get("cookie")?.includes("dark");
       return html.replace("###theme-class###", isDark ? "dark" : "");
     },
   });

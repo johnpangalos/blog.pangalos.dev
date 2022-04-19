4af65bf192f359e2156010c1cea588b24be19266
John Pangalos <john@pangalos.dev>
2022-04-19 22:35:16 +0200

    Tooltip!

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { browser } from "$app/env";
   let open: boolean = false;
   let openFunc = () => (open = true);
+  let el: HTMLElement;
+  let classXTranslate: string = "-translate-x-1/2";
 
   if (browser) {
-    window.document.addEventListener(
-      "touchstart",
-      () => open && (open = false),
-      { capture: true, passive: true }
-    );
+    onMount(() => {
+      const updateXPosition = () => {
+        const { right, left } = el.getBoundingClientRect();
+        if (right > window.document.body.clientWidth)
+          return (classXTranslate = "-translate-x-[80%]");
+        if (left < 0) return (classXTranslate = "-translate-x-[20%]");
+        classXTranslate = "-translate-x-1/2";
+      };
+      updateXPosition();
+      window.addEventListener("resize", updateXPosition);
+
+      window.document.addEventListener(
+        "touchstart",
+        () => open && (open = false),
+        { capture: true, passive: true }
+      );
+    });
   }
 </script>
 
-<span class="relative inline-block">
+<span class="relative inline-flex">
   <span
     class="border-b-2 border-dotted border-fuchsia-700"
     on:touchstart={openFunc}
@@ -21,12 +36,13 @@
   >
     <slot name="main" />
   </span>
-  {#if open}
-    <span
-      class="absolute left-1/2 -translate-x-1/2 -translate-y-full whitespace-nowrap rounded bg-fuchsia-700 px-2 text-center text-white"
-      on:blur={() => (open = false)}
-    >
-      <slot name="hover" />
-    </span>
-  {/if}
+  <span
+    bind:this={el}
+    class:visible={open}
+    class:invisible={!open}
+    class={`absolute left-1/2 -mr-[200px] max-w-[180px] -translate-y-full rounded bg-fuchsia-700 py-2 px-2 text-center text-white ${classXTranslate}`}
+    on:blur={() => (open = false)}
+  >
+    <slot name="hover" />
+  </span>
 </span>

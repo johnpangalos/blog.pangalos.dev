97068a98b21b98fa034a094d3019f5fa3a6e79c7
John Pangalos <john@pangalos.dev>
2022-08-12 21:11:13 +0200

    another try to fix the title

--- src/routes/blog/__layout.svelte
+++ src/routes/blog/__layout.svelte
@@ -1,3 +1,19 @@
+<script context="module">
+  /** @type {import('./__types/[slug]').Load} */
+  export async function load({ params, fetch, session, stuff }) {
+    return {
+      status: response.status,
+      props: {
+        slug: response.ok && params.slug,
+      },
+    };
+  }
+</script>
+
+<svelte:head>
+  <title>{$$props.slug}</title>
+</svelte:head>
+
 <div class="prose prose-slate pt-10 pb-20 dark:prose-invert lg:prose-lg">
   <slot />
 </div>

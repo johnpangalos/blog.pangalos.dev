ad987113bec6055563dba21e65ef8f7a889a93e3
John Pangalos <john@pangalos.dev>
2022-08-12 21:18:48 +0200

    copy from svelte kit

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,15 +1,27 @@
 <script context="module">
-  /** @type {import('./__types/[slug]').Load} */
-  export async function load({ params, fetch, session, stuff }) {
+  // TODO should use a shadow endpoint instead, need to fix a bug first
+  /** @type {import('@sveltejs/kit').Load} */
+  export async function load({ fetch, params }) {
+    const res = await fetch(`/blog/${params.slug}.json`);
+    const { prev, next, section } = await res.json();
+
     return {
-      status: response.status,
       props: {
-        slug: response.ok && params.slug,
+        prev,
+        next,
+        section,
       },
     };
   }
 </script>
 
 <svelte:head>
-  <title>{$$props.slug}</title>
+  <title>{section.title} • blog.pangalos.dev</title>
+
+  <meta name="twitter:title" content="blog.pangalos.dev" />
+  <meta
+    name="twitter:description"
+    content="{section.title} • blog.pangalos.dev"
+  />
+  <meta name="Description" content="{section.title} • blog.pangalos.dev" />
 </svelte:head>
